b071321cc7d29976bf036ef38a0bd4e7c4f9e676
shumakovd <shumakov_d@hotmail.com>
2022-12-09 17:29:40 +0200

    Initial Commit

---
+++ Template Method/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Template Method
+//
+//  Created by Shumakov Dmytro on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
